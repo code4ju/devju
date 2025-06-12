f71dc09ec88350ef269bcc22464c1c09eb957693
Ju Chang <code4ju@gmail.com>
2025-06-12 14:41:08 +0200

    add german introduction text

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,6 +8,12 @@ export const Hero = () => {
 			"In the past I started by creating content for a startup. And after learning different skills ranging from product management to maintaining the wordpress-webshop, I proposed and executed the redesigning for the companies webshop, resulting in a 200% increase in profit over 2 years and stronger customer retention.",
 			"In my spare time, I'm learning python and staying active, teaching and dancing salsa and ballroom.",
 		],
+		[
+			"Ich bin ein Full-Stack-Developer in Ausbildung, mit dem Fokus auf sauberen, skalierbaren Anwendungen. Mein Spezialgebiet liegt in der Entwicklung pixelgenauer Frontends und stabiler Backend-Architekturen – unter Einsatz von JavaScript, React, Node.js und Express.",
+			"Nach meinem 12-monatigen Full-Stack-Development-Programm am DCI – Digital Career Institute – konnte ich meine Fähigkeiten bei Munich Software Studios weiter vertiefen. Dort arbeitete ich an mehreren Projekten mit React, Vue.js und Next.js mit, entwickelte wiederverwendbare Komponenten und unterstützte produktionsreife Deployments – alles in agilen Teams.",
+			"Zuvor war ich bei einem Startup tätig, wo ich zunächst Inhalte erstellte und verschiedene Aufgaben übernahm – vom Produktmanagement bis zur Pflege des WordPress-Shops. Schließlich schlug ich eine komplette Neugestaltung des Webshops vor und setzte diese erfolgreich um. Das Ergebnis: Eine Umsatzsteigerung von 200 % innerhalb von zwei Jahren sowie deutlich höhere Kundenbindung.",
+			"In meiner Freizeit lerne ich Python und bleibe aktiv – ich unterrichte und tanze Salsa sowie Standard- und Lateinamerikanische Tänze.",
+		],
 	];
 
 	const chapters = [
